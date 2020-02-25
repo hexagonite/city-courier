4c56a53e724d4d439d1f9ffec38cddb77be6e8db
Cezary Krawczyk <cezary.k.krawczyk@gmail.com>
2020-02-25 11:15:05 +0100

    init commit

---
+++ src/main/java/pl/ug/citycourier/CityCourierApplication.java
@@ -0,0 +1,13 @@
+package pl.ug.citycourier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityCourierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityCourierApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/ug/citycourier/CityCourierApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ug.citycourier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityCourierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
