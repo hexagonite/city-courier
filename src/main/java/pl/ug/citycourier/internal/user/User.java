c27c82ded5d3094bcf94f83f4f6b9136ca93c108
Cezary Krawczyk <cezary.k.krawczyk@gmail.com>
2020-03-01 13:23:13 +0100

    #6 - added user + security

--- src/main/java/pl/ug/citycourier/internal/user/User.java
+++ src/main/java/pl/ug/citycourier/internal/user/User.java
@@ -1,4 +1,93 @@
 package pl.ug.citycourier.internal.user;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import pl.ug.citycourier.internal.security.entity.Role;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Entity()
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @NotBlank(message = "Username is mandatory")
+    private String username;
+    @Email
+    @NotBlank(message = "Email is mandatory")
+    private String email;
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+    private String secondName;
+    private String password;
+    @NotBlank(message = "Surname is mandatory")
+    private String surname;
+    @JsonIgnore
+    @ManyToOne
+    private Role role;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
---
+++ src/main/java/pl/ug/citycourier/internal/user/UserBuilder.java
@@ -0,0 +1,74 @@
+package pl.ug.citycourier.internal.user;
+
+import pl.ug.citycourier.internal.security.entity.Role;
+
+public final class UserBuilder {
+    private long id;
+    private String username;
+    private String email;
+    private String name;
+    private String secondName;
+    private String password;
+    private String surname;
+    private Role role;
+
+    private UserBuilder() {
+    }
+
+    public static UserBuilder anUser() {
+        return new UserBuilder();
+    }
+
+    public UserBuilder withId(long id) {
+        this.id = id;
+        return this;
+    }
+
+    public UserBuilder withUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public UserBuilder withEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public UserBuilder withName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public UserBuilder withSecondName(String secondName) {
+        this.secondName = secondName;
+        return this;
+    }
+
+    public UserBuilder withPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public UserBuilder withSurname(String surname) {
+        this.surname = surname;
+        return this;
+    }
+
+    public UserBuilder withRole(Role role) {
+        this.role = role;
+        return this;
+    }
+
+    public User build() {
+        User user = new User();
+        user.setId(id);
+        user.setUsername(username);
+        user.setEmail(email);
+        user.setName(name);
+        user.setSecondName(secondName);
+        user.setPassword(password);
+        user.setSurname(surname);
+        user.setRole(role);
+        return user;
+    }
+}
