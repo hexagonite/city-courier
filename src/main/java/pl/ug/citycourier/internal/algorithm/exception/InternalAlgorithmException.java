4cdb457c5ef3c06a1b3ef2513860163200e27c9e
Cezary Krawczyk <ckrawczyk@atlassian.com>
2020-05-07 17:50:04 +0200

    #25 - repairing courier job creator

--- src/main/java/pl/ug/citycourier/internal/algorithm/exception/InternalAlgorithmException.java
+++ src/main/java/pl/ug/citycourier/internal/algorithm/exception/InternalAlgorithmException.java
@@ -1,6 +1,6 @@
 package pl.ug.citycourier.internal.algorithm.exception;
 
-public class InternalAlgorithmException extends Exception {
+public class InternalAlgorithmException extends RuntimeException {
 
     public InternalAlgorithmException(String message) {
         super("Internal Algorithm exception: " + message);
